be6f42b6d8fa4966b7c478252076865561c04902
Brittany jack <jackbrittany54@yahoo.com>
2018-09-20 15:00:18 -0600

    Deep Dive Fundamentals project js

---
+++ variables-and-data.js
@@ -0,0 +1,10 @@
+/*
+* Variables, Data Types, and Typing
+ */
+let wordData= "Word List";
+
+console.log(wordData);
+
+let subTotal = 100;
+let tax = subTotal * 0.07;
+let totalWithTax = tax + subTotal;
